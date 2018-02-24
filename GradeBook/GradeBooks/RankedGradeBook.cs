3ee91e46653146aeb942687356a21271c3959293
AndrewTilson <awtilso@gmail.com>
2018-02-24 16:57:16 -0600

    Update grade letter return for rankedgradebook.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -9,6 +10,39 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             this.Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (this.Students.Count < 5)
+                throw new InvalidOperationException("At least 5 students required for ranked grading");
+
+            var divisible = Students.Count / 5;
+
+            var sort = Students.OrderByDescending(x => x.AverageGrade).ToList();
+            var rank = sort.FindIndex(x => x.AverageGrade == averageGrade) + 1 / divisible;
+
+
+            if (rank <= 1)
+            {
+                return 'A';
+            }
+            else if(rank <= 2)
+            {
+                return 'B';
+            }
+            if (rank <= 3)
+            {
+                return 'C';
+            }
+            if (rank <= 4)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
         }
     }
 }
